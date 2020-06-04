e29051e2d8d70c29fffca4f2c74fa6a034af32a6
Felipe VanWinkle <interngithub@gmail.com>
2020-06-04 09:28:24 -0600

    changed the format to push data to one topic

--- gamepad/gamepad/subscriber.py
+++ gamepad/gamepad/subscriber.py
@@ -8,31 +8,10 @@ class MinimalSubscriber(Node):
 
     def __init__(self):
         super().__init__('minimal_subscriber')
-        self.subscription = self.create_subscription(String,'abxy',self.colorfulbuttons_callback,10)
-        self.subscription = self.create_subscription(String, 'lt/rt',self.triggers_callback,10)
-        self.subscription = self.create_subscription(String, 'lb/rb',self.nubs_callback,10)
-        self.subscription = self.create_subscription(String, 'dpad',self.dpad_callback,10)
-        self.subscription = self.create_subscription(String, 'misc',self.misc_callback,10)
-        self.subscription = self.create_subscription(String, 'analog',self.analog_callback,10)
-        self.subscription 
+        self.subscription = self.create_subscription(String,'events',self.callback,10)
 
-    def colorfulbuttons_callback(self, msg):
+    def callback(self, msg):
         self.get_logger().info('Current Value: "%s"' % msg.data)
-
-    def triggers_callback(self, msg_triggers):
-        self.get_logger().info('Percentile Value: "%s"' % msg_triggers.data)
-
-    def nubs_callback(self, msg_nubs):
-        self.get_logger().info('Current Value: "%s"' % msg_nubs.data)
-
-    def dpad_callback(self, msg_dpad):
-        self.get_logger().info('Current Value: "%s"' % msg_dpad.data)
-
-    def misc_callback(self, msg_misc):
-        self.get_logger().info('Current Value: "%s"' % msg_misc.data)
-
-    def analog_callback(self, msg_joy):
-        self.get_logger().info('Current Value: "%s"' % msg_joy.data)
 
 
 def main(args=None):
